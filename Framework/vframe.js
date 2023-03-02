11795aaed4f8ff211ceda17d04d73e9b951c12ec
Voxalium <voxalium@gmail.com>
2023-03-02 11:51:27 +0100

    add framework

---
+++ Framework/vframe.js
@@ -0,0 +1,13 @@
+addEventListener("DOMContentLoaded", () =>{
+    
+    const col  = document.createElement('script');
+    col.type = "text/javascript";
+    col.src  = "collision.js";
+    document.head.appendChild(col);
+
+    const gfx  = document.createElement('script');
+    gfx.type = "text/javascript";
+    gfx.src  = "gfx.js";
+    document.head.appendChild(gfx);
+
+})
