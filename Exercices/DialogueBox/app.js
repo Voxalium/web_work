18fd6df880943c6c3bd55669be33f68fd21bdd21
Voxalium <voxalium@gmail.com>
2023-03-18 12:41:05 +0100

    add movement in DialogueBox

--- Exercices/DialogueBox/app.js
+++ Exercices/DialogueBox/app.js
@@ -8,14 +8,14 @@ const dialogueBox1 = new obj.rectangle(cv.canvas.width/2 - 250  , cv.canvas.heig
 let dial = ["Hello", "How are you ?"]
 let i = 0;
 
+let vx = 0;
+let vy = 0;
 
-gfx.drawRect(10,10,10,10,"black", cv.ctx);
+let sqr = new obj.rectangle(10,10,10,10);
 
 function drawDialogue(text){
-    
     gfx.drawRectObj(dialogueBox1, cv.ctx);
     gfx.drawText(text, dialogueBox1.x + 5, dialogueBox1.y + 20, 18, "white", cv.ctx)
-
 }
 
 document.body.addEventListener("keypress",(e) =>{
@@ -24,10 +24,35 @@ document.body.addEventListener("keypress",(e) =>{
     }
 })
 
-function Game(){
-    requestAnimationFrame(Game);
-    console.log(i);
-    drawDialogue(dial[i]);
+function Draw(){
+    gfx.clearScreen(cv.ctx, cv.canvas);
+    gfx.drawRect(sqr.x, sqr.y, sqr.w, sqr.h,"black", cv.ctx);
+}
 
+function Update(){
+    move();
+}
+
+function Game(){
+    Update();
+    Draw();
+    drawDialogue(dial[i]);
+    requestAnimationFrame(Game);
 }
 requestAnimationFrame(Game);
+
+function move(){
+    document.body.addEventListener("keypress", (e) =>{
+        if(     e.code == "KeyW") vy = -1; 
+        else if(e.code == "KeyS") vy =  1;
+        else if(e.code == "KeyA") vx = -1;
+        else if(e.code == "KeyD") vx =  1;})
+    document.body.addEventListener("keyup", (e) =>{
+        if(     e.code == "KeyW") vy =  0; 
+        else if(e.code == "KeyS") vy =  0;
+        else if(e.code == "KeyA") vx =  0;
+        else if(e.code == "KeyD") vx =  0;})
+
+    sqr.x += vx;
+    sqr.y += vy;
+}
