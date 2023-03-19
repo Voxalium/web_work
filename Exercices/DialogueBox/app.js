3fd7f9b077a7f0e0263f176e542ef67b809e6b71
Voxalium <voxalium@gmail.com>
2023-03-19 11:51:19 +0100

    add dial.json /only work with chrome(assert json)

--- Exercices/DialogueBox/app.js
+++ Exercices/DialogueBox/app.js
@@ -1,11 +1,10 @@
 import * as gfx from "../../Framework/gfx.js";
 import * as obj from "../../Framework/obj.js";
+import data from './dial.json' assert {type: 'json'};
 
 const cv = gfx.createCanvas(600,400);
 const dialogueBox1 = new obj.rectangle(cv.canvas.width/2 - 250  , cv.canvas.height - 135, 500, 125);
 
-//let t1 = new obj.text("Hello World");
-let dial = ["Hello", "How are you ?"]
 let i = 0;
 
 let vx = 0;
@@ -36,7 +35,7 @@ function Update(){
 function Game(){
     Update();
     Draw();
-    drawDialogue(dial[i]);
+    drawDialogue(data[Object.keys(data)[i]]);
     requestAnimationFrame(Game);
 }
 requestAnimationFrame(Game);
