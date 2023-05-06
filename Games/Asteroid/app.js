1e2e8da834632f8755331caf6233243da71263fd
Voxalium <voxalium@gmail.com>
2023-05-06 14:06:35 +0200

    move asteroid

--- Games/Asteroid/app.js
+++ Games/Asteroid/app.js
@@ -11,11 +11,23 @@ let player = {
 const playerSprite = new Image();
 playerSprite.src = player.sprite;
 
+function Event(){
+    window.addEventListener("keydown", (e)=>{
+        switch( e.code){
+            case "ArrowUp":
+                player.vy = -1
+
+        }
+    
+    })
+}
+Event();
 function drawPlayer(){
     cv.ctx.drawImage(playerSprite, player.x, player.y);
 }
 
 function Update(){
+    player.y += player.vy;
 }
 
 function Draw(){
