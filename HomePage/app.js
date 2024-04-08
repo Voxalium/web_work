a683725c69661528cbe740339fbc9c6fd2945f07
Voxalium <voxalium@gmail.com>
2024-04-08 12:41:00 +0200

    update home

--- HomePage/app.js
+++ HomePage/app.js
@@ -1,37 +1,21 @@
+addEventListener("DOMContentLoaded", () => {
+  document.body.style.background = "url(img/wall.webp)";
 
+  //BACKGROUND
 
-addEventListener("DOMContentLoaded",() =>{
+  //TIME
+  const dateContainer = document.getElementById("date");
+  const time = document.createElement("div");
+  const dateTxt = document.createElement("div");
 
-    const wiki = document.getElementById("wiki");
-    const BGCOLOR = "rgba(0,0,0,0.5)"
-    
-    // document.body.style.background= "url(https://picsum.photos/3840/2160) no-repeat center center fixed "
-    document.body.style.background = "url(img/wall.png)";
-    document.body.style.backgroundSize = "cover";
-    
-    //BACKGROUND
-    
+  dateContainer.appendChild(dateTxt);
+  dateContainer.appendChild(time);
 
-    //TIME
-    const dateContainer = document.getElementById("date");
-    const time = document.createElement("div");
-    const dateTxt = document.createElement("div");
-
-    dateContainer.appendChild(dateTxt);
-    dateContainer.appendChild(time);
-
-
-    function setTime()
-    {
+  function setTime() {
     let timeF = showTime();
-    time.innerHTML =  timeF[0];
+    time.innerHTML = timeF[0];
     dateTxt.innerHTML = timeF[1];
-    timeF = setTimeout(setTime,1000);
-    }
-    setTime();
-    
-    const requestBar = document.getElementById("requestBar");
-
-    const links = document.getElementById("links");
-
+    timeF = setTimeout(setTime, 1000);
+  }
+  setTime();
 });
