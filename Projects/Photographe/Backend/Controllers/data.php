ff9a5a27cdbef74c58155d46fe09ea9b8c216f03
Voxalium <voxalium@gmail.com>
2024-04-24 12:12:53 +0200

    server route-> /Img/ | display img on frontend

--- Projects/Photographe/Backend/Controllers/data.php
+++ Projects/Photographe/Backend/Controllers/data.php
@@ -13,6 +13,8 @@ function getData()
 
 }
 
+
+
 function converter($photo)
 {
     return [
--- Projects/Photographe/Backend/index.php
+++ Projects/Photographe/Backend/index.php
@@ -3,22 +3,48 @@
 require "./Controllers/data.php";
 $uri = $_SERVER["REQUEST_URI"];
 
-switch($uri) {
-    case "/":
-        echo "Hello from / \n";
-        break;
-    case "/api/login":
-        echo "Hello from /api/login";
-        break;
-    case "/api/datas":
-        header("Content-Type: application/json");
-        $data = getData();
-        $jsonData = [];
-        foreach($data as $d) {
-            $jsonData[] = $d;
-        }
-        echo  json_encode($jsonData);
-        break;
-    default:
-        echo "404";
+
+function getImage($imageName)
+{
+    $imagePath = "./Img/" . $imageName;
+
+    if(file_exists($imagePath)) {
+        header("Content-Type: image/webp");
+        readfile($imagePath);
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        echo "404 - Image Not Found";
+    }
+
+}
+
+function handleAPIRequest($uri)
+{
+    switch($uri) {
+        case "/":
+            echo "Hello from / \n";
+            break;
+        case "/api/login":
+            echo "Hello from /api/login";
+            break;
+        case "/api/datas":
+            header("Content-Type: application/json");
+            $data = getData();
+            $jsonData = [];
+            foreach($data as $d) {
+                $jsonData[] = $d;
+            }
+            echo  json_encode($jsonData);
+            break;
+        default:
+            header("HTTP/1.0 404 Not Found");
+            echo "404 Page Not Found";
+    }
+}
+
+if(strpos($uri, "/img/") === 0) {
+    $imageName = basename($uri);
+    getImage($imageName);
+} else {
+    handleAPIRequest($uri);
 }
