497aac6f63d9bc7be1d60b9abb0d7677fd2ec8fc
Voxalium <voxalium@gmail.com>
2024-05-21 15:00:01 +0200

    start portfolio v3

---
+++ Portfolio/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div className="Home flex col fullcenter">
+      <h1>Bonjour!</h1>
+      <div className="text textcenter mt1">
+        Je suis Terence Truyens,
+        <br /> j'utilise aussi le pseudo Voxalium.
+      </div>
+    </div>
+  );
+}
+
+export default Home;
