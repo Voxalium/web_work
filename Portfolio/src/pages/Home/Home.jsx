a7d3da7e5f829899ed4298a41e3068db8b6fe074
Voxalium <voxalium@gmail.com>
2024-05-22 12:32:12 +0200

    add border anim HOME

--- Portfolio/src/pages/Home/Home.jsx
+++ Portfolio/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 function Home() {
   return (
-    <div className="Home flex col fullcenter">
-      <h1>Bonjour!</h1>
-      <div className="text textcenter mt1">
-        Je suis Terence Truyens,
-        <br /> j'utilise aussi le pseudo Voxalium.
+    <div className="Home row fullcenter ">
+      <div className="outer br12 col fullcenter">
+        <div className="inner bg br12 pd15 col fullcenter">
+          <h1>Bonjour!</h1>
+          <div className="text textcenter mt1">
+            Je suis Terence Truyens,
+            <br /> j'utilise aussi le pseudo Voxalium.
+          </div>
+        </div>
       </div>
     </div>
   );
