881f3cb4dc829693cc5dadf8786b7d956f502e7d
Voxalium <voxalium@gmail.com>
2024-05-22 15:24:12 +0200

    add mouse 3d rotation effect

--- Portfolio/src/pages/Home/Home.jsx
+++ Portfolio/src/pages/Home/Home.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+import mouseAim from "../../scripts/mouseAim";
+
 function Home() {
+  useEffect(() => {
+    mouseAim(".Home", ".outer");
+  });
   return (
     <div className="Home row fullcenter ">
       <div className="outer br12 col fullcenter">
@@ -7,6 +13,7 @@ function Home() {
           <div className="text textcenter mt1">
             Je suis Terence Truyens,
             <br /> j'utilise aussi le pseudo Voxalium.
+            <br /> Je suis spécialisé dans le développement web frontend.
           </div>
         </div>
       </div>
---
+++ Portfolio/src/scripts/mouseAim.js
@@ -0,0 +1,37 @@
+function mouseAim(containerTag, innerTag) {
+  const container = document.querySelector(containerTag);
+  const inner = document.querySelector(innerTag);
+  const constrain = 300;
+  console.log(container);
+  console.log(inner);
+
+  function transform(x, y, e) {
+    let box = e.getBoundingClientRect();
+    let calcX = -(y - box.y - box.height) / constrain;
+    let calcY = (x - box.x - box.width) / constrain;
+
+    return (
+      "perspective(80px) " +
+      "   rotateX(" +
+      calcX +
+      "deg) " +
+      "   rotateY(" +
+      calcY +
+      "deg) "
+    );
+  }
+  function transformElement(e, xyE) {
+    e.style.transform = transform.apply(null, xyE);
+  }
+
+  container.onmousemove = function (e) {
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([inner]);
+
+    window.requestAnimationFrame(function () {
+      transformElement(inner, position);
+    });
+  };
+}
+
+export default mouseAim;
